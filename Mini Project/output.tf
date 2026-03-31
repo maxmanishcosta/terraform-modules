80fd676ea753ee4f8a2a0d58749bd2a945ea28b7
Manish Kumar <64778172+maxmanishcosta@users.noreply.github.com>
2026-03-31 23:00:42 +0530

    Add output variables for AWS instance details

---
+++ Mini Project/output.tf
@@ -0,0 +1,18 @@
+vi output.tf
+
+output "instance_public_ip" {
+  value      = aws_instance.myinstance.public_ip
+  sensitive  = true
+}
+
+output "instance_id" {
+  value = aws_instance.myinstance.id
+}
+
+output "instance_public_dns" {
+  value = aws_instance.myinstance.public_dns
+}
+
+output "instance_arn" {
+  value = aws_instance.myinstance.arn
+}
