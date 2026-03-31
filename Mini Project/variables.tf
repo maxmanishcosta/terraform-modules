b13b229c2bf24196cf0a452d1c9134bfc405c2b5
Manish Kumar <64778172+maxmanishcosta@users.noreply.github.com>
2026-03-31 22:58:28 +0530

    Create variables.tf

---
+++ Mini Project/variables.tf
@@ -0,0 +1,17 @@
+vi variables.tf
+
+variable "ami" {
+  description = "passing ami value"
+  type        = string
+  default     = ""
+}
+
+variable "instance_type" {
+  type    = string
+  default = ""
+}
+
+variable "key_name" {
+  type    = string
+  default = ""
+}
